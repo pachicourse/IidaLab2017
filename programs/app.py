952eb394e5768c13a39c145236649b2b7692b075
pachicourse <ddds78199500825@gmail.com>
2017-06-07 19:38:38 +0000

    relay_message

--- programs/app.py
+++ programs/app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request
 import os
+import requests
+import json
 
 ROOT_ADDRESS = os.environ.get('ROOT_ADDRESS')
+ROOT_PORT = os.environ.get('ROOT_PORT')
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,9 +16,13 @@ def hello():
 def relay_message():
     if request.headers['Content-Type'] != 'application/json':
         print(request.headers['Content-Type'])
-    
+        return 'It is not json data.'
     print(request.json)
     print(request.json['message'])
+    url = 'http://' + ROOT_ADDRESS + ':' + ROOT_PORT + '/resque'
+    r = requests.post(url, json.dumps(request.json), \
+                      headers={'Content-Type':'application/json'})
+    print(r)
     return 'done'
 
 if __name__ == '__main__':
