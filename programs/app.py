47581be79aa444d39324dd5df1351da602fb0c3e
pachicourse <ddds78199500825@gmail.com>
2017-06-07 18:55:16 +0000

    print json

--- programs/app.py
+++ programs/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import os
 
 ROOT_ADDRESS = os.environ.get('ROOT_ADDRESS')
 app = Flask(__name__)
@@ -6,6 +7,15 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return render_template('index.html')
+
+@app.route('/resque', methods=['POST'])
+def relay_message():
+    if request.headers['Content-Type'] != 'application/json':
+        print(request.headers['Content-Type'])
+    
+    print(request.json)
+    print(request.json['message'])
+    return 'done'
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=False, threaded=True)
