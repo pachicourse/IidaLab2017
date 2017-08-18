45f9ecb3b94d2433522b8ed03942ac05fa4c1148
pachicourse <ddds78199500825@gmail.com>
2017-08-18 14:06:55 +0900

    add formCheck() function

---
+++ programs/static/js/formCheck.js
@@ -0,0 +1,25 @@
+function checkForm(){
+    var flag = 0
+    //formの内容をチェック
+    if(document.form.name.value == ""){
+        flag = 1
+    }
+    else if(document.form.location.value == ""){
+        flag = 1
+    }
+    else if(document.form.situation.value == ""){
+        flag = 1
+    }
+    //検査
+    if(flag){
+        window.alert('必須項目に未入力がありました') // 入力漏れがあれば警告ダイアログを表示
+        return false // 送信を中止
+    }
+    else{
+        if(!window.confirm('送信してよろしいですか？')){ // 確認ダイアログを表示
+            window.alert('キャンセルされました') // 警告ダイアログを表示
+            return false
+        }
+        return true // 送信を実行
+    }
+}
