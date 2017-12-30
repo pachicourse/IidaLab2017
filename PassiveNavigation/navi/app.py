279cd11f9db775699af5634cdf2e47caac48ad67
pachicourse <ddds78199500825@gmail.com>
2017-12-30 15:26:49 +0900

    methodsの位置が違うんだよ間抜け

--- PassiveNavigation/navi/app.py
+++ PassiveNavigation/navi/app.py
@@ -1,22 +1,39 @@
 import json
+# import wiringpi as wp
 from flask import Flask, request
 
+#適当 あとで決める
+LED_PIN = 21
 app = Flask(__name__)
 
 class Navi:
     def __init__(self, needs_data):
         self.needs_data = needs_data
 
-@app.route('/')
-def recieve_store_data(methods=['GET','POST']):
-    if reqest.method == 'POST':
+    def check_needs(self, posted_data):
+        return
+
+@app.route('/', methods=['GET', 'POST'])
+def recieve_store_data():
+    if request.method == 'POST':
+        posted_data = request.json
         store_name = request.json['name']
         store_type = request.json['type']
         store_keyword = request.json['keywords']
 
+        print(navi.needs_data)
+        print(posted_data)
+        # navi.check_needs
+        return 'hello'
+
     if request.method == 'GET':
+        return
 
 if __name__ == '__main__':
+    # wp.wiringPiSetupGpio()
+    # # LED_PINを出力用にセット
+    # wp.pinMode(LED_PIN, 1)
+
     try:
         # ローカルJSONファイルの読み込み
         with open('navi_data.json', 'r') as f:
